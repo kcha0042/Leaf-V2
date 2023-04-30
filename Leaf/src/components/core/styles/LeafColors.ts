fedb35a54345e4c98aeba16a4377acf8294dffc7
Andre Pham <andrepham33@gmail.com>
2023-05-01 09:59:20 +1000

    Added textError color, and updated LeafColors class header

--- Leaf/src/components/core/styles/LeafColors.ts
+++ Leaf/src/components/core/styles/LeafColors.ts
@@ -2,18 +2,21 @@ import LeafColor from "./color/LeafColor";
 
 /**
  * Predefined colors to be used application-wide.
- * Colors are all defined with a light-mode and dark-mode variant.
+ * Colors are defined with a light-mode and an optional dark-mode variant.
  * Colors should be named using light-mode convention, that is, colors should be named according to how they should be read in light mode. "Dark text" is text that is dark in light mode, and light in dark mode.
+ * To select colors, I recommend the resource: https://yeun.github.io/open-color/
  */
 class LeafColors {
 
     // Palette
 
     static get accent(): LeafColor {
+        // TODO: Update for dark mode
         return new LeafColor("#51b962");
     }
 
     static get lightAccent(): LeafColor {
+        // TODO: Update for dark mode
         return new LeafColor("#e8f9e9");
     }
 
@@ -27,13 +30,20 @@ class LeafColors {
         return new LeafColor("#f8f9fa", "#212529");
     }
 
+    static get textError(): LeafColor {
+        // TODO: Update for dark mode
+        return new LeafColor("#e03131");
+    }
+
     // Backgrounds
 
     static get textBackgroundDark(): LeafColor {
+        // TODO: Update for dark mode
         return new LeafColor("#ececec");
     }
 
     static get textBackgroundLight(): LeafColor {
+        // TODO: Update for dark mode
         return new LeafColor("#fefbfe");
     }
 
