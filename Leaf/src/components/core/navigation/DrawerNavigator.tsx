2bf0c0953f401a069a3ebd4f0b35837bfe292d58
Antony <86053464+AntonyLoose@users.noreply.github.com>
2023-05-11 20:36:22 +1000

    Fix drawer orientation change bug

--- Leaf/src/components/core/navigation/DrawerNavigator.tsx
+++ Leaf/src/components/core/navigation/DrawerNavigator.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react"
-import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from "@react-navigation/drawer"
+import React, { useEffect, useState } from "react"
+import { createDrawerNavigator } from "@react-navigation/drawer"
 import { LeafStack } from "./Types"
 import { StackWrapper } from "./RenderStack"
 import LeafColors from "../styles/LeafColors"
 import Environment from "../../../state/environment/Environment"
 import { LeafScreenOrientation } from "../../../state/environment/types/LeafScreenOrientation"
 import { Dimensions } from "react-native"
+import { DrawerActions } from '@react-navigation/native';
+import { useNavigation } from '@react-navigation/native';
 
 interface Props {
     stacks: LeafStack[]
@@ -15,15 +17,25 @@ const Drawer = createDrawerNavigator()
 
 export const DrawerNavigator: React.FC<Props> = ({ stacks }) => {
 
-    const getDrawerType = (): "front" | "slide" | "back" | "permanent" => {
-        return Environment.instance.getScreenOrientation() == LeafScreenOrientation.Landscape ? "permanent" : "slide";
+    const getDrawerType = (): 'front' | 'slide' | 'back' | 'permanent' => {
+        return Environment.instance.getScreenOrientation() == LeafScreenOrientation.Landscape ? 'permanent' : 'front';
     }
-
     const [ drawerType, setDrawerType ] = useState(getDrawerType());
+    const navigation = useNavigation();
 
-    Dimensions.addEventListener('change', () => {
-        setDrawerType(getDrawerType)
-    })
+    // We only want to add the listener once, when the drawer is mounted
+    useEffect(() => {
+        const handleDimensionChange = () => {
+            const newDrawerType = getDrawerType();
+            setDrawerType(newDrawerType);
+
+            if (newDrawerType !== 'permanent') {
+                navigation.dispatch(DrawerActions.toggleDrawer());
+            }
+        };
+
+        Dimensions.addEventListener('change', handleDimensionChange)
+    }, [])
 
     return (
         <Drawer.Navigator
