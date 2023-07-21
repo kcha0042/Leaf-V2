7ad3458a4f91dbadb461bd6acac55b95ab6de59f
Andre Pham <andrepham33@gmail.com>
2023-07-21 15:48:16 +1000

    FIxed navigation prop

--- Leaf/src/components/screens/ActionsScreen.tsx
+++ Leaf/src/components/screens/ActionsScreen.tsx
@@ -3,12 +3,12 @@ import LeafText from "../base/LeafText/LeafText";
 import LeafTypography from "../styling/LeafTypography";
 import { ScrollView, View } from "react-native";
 import LeafColors from "../styling/LeafColors";
-import { PatientsNavigationProp } from "../navigation/Params";
 import VStack from "../containers/VStack";
 import LeafDimensions from "../styling/LeafDimensions";
+import { NavigationProp, ParamListBase } from "@react-navigation/native";
 
 interface Props {
-    navigation?: PatientsNavigationProp;
+    navigation?: NavigationProp<ParamListBase>;
 }
 
 const ActionsScreen: React.FC<Props> = ({ navigation }) => {
