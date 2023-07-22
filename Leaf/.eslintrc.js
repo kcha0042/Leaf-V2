0607ba1851322cf5a5a1d7b08a9fc86968ba19e2
Andre Pham <andrepham33@gmail.com>
2023-07-22 19:56:48 +1000

    Fixed linting rule to be ts specific

--- Leaf/.eslintrc.js
+++ Leaf/.eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
         "react/react-in-jsx-scope": "off",
         "react/jsx-uses-react": "error",
         "react/jsx-uses-vars": "error",
-        "no-unused-vars": "warn",
+        "@typescript-eslint/no-unused-vars": "warn",
         "react/prop-types": "off",
         "react-native/split-platform-components": "off",
     },
