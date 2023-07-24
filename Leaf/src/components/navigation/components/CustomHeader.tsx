b530774e83fffbaaf58dd6de70d832748b179d1c
Andre Pham <andrepham33@gmail.com>
2023-07-24 12:26:13 +1000

    Typo fixes

--- Leaf/src/components/navigation/components/CustomHeader.tsx
+++ Leaf/src/components/navigation/components/CustomHeader.tsx
@@ -34,7 +34,7 @@ const LeafHeader: React.FC<CustomLeafHeaderProps> = ({ title, buttonProps }) => 
                     }}
                     style={styles.backButton}
                 >
-                    <Icon name={"chevron-left"} size={45} colour={"#007AFF"} style={{ marginLeft: -10 }} />
+                    <Icon name={"chevron-left"} size={45} color={LeafTypography.headerScreen.color} style={{ marginLeft: -10 }} />
                 </TouchableOpacity>
             ) : null}
             <LeafText style={{ flex: 1 }} typography={LeafTypography.headerScreen}>
