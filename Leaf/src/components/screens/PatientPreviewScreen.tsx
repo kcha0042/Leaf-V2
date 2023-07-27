f0e04f83d2af76fb7ebb7b36c5d7028e53a08286
Antony <86053464+AntonyLoose@users.noreply.github.com>
2023-07-27 20:22:24 +1000

    Base for Patient preview screen

--- Leaf/src/components/screens/PatientPreviewScreen.tsx
+++ Leaf/src/components/screens/PatientPreviewScreen.tsx
@@ -1,9 +1,12 @@
 import { NavigationProp, ParamListBase } from "@react-navigation/native";
 import React from "react";
+import LeafIcon from "../base/LeafIcon/LeafIcon";
 import LeafText from "../base/LeafText/LeafText";
+import FlatContainer from "../containers/FlatContainer";
+import HStack from "../containers/HStack";
 import VStack from "../containers/VStack";
+import LeafColors from "../styling/LeafColors";
 import LeafDimensions from "../styling/LeafDimensions";
-import LeafTypography from "../styling/LeafTypography";
 import DefaultScreenContainer from "./containers/DefaultScreenContainer";
 
 interface Props {
@@ -11,6 +14,8 @@ interface Props {
 }
 
 const PatientPreviewScreen: React.FC<Props> = ({ navigation }) => {
+    const iconSize = 30;
+
     return (
         <DefaultScreenContainer>
             <VStack
@@ -19,7 +24,34 @@ const PatientPreviewScreen: React.FC<Props> = ({ navigation }) => {
                     flex: 1,
                 }}
             >
-                <LeafText typography={LeafTypography.body}>TODO: Patient Preview</LeafText>
+                <LeafIcon icon={"clock-outline"} size={iconSize} color={LeafColors.textDark} />
+                <FlatContainer style={{ flex: 1, width: "100%" }}>
+                    <LeafText> TODO </LeafText>
+                </FlatContainer>
+
+                <HStack>
+                    <LeafIcon icon={"account"} size={iconSize} color={LeafColors.textDark} />
+                    <LeafText wide={false}> Identity </LeafText>
+                </HStack>
+                <FlatContainer style={{ flex: 1, width: "100%" }}></FlatContainer>
+
+                <HStack>
+                    <LeafIcon icon={"information-outline"} size={iconSize} color={LeafColors.textDark} />
+                    <LeafText wide={false}> Bio </LeafText>
+                </HStack>
+                <FlatContainer style={{ flex: 1, width: "100%" }}></FlatContainer>
+
+                <HStack>
+                    <LeafIcon icon={"file-document-edit"} size={iconSize} color={LeafColors.textDark} />
+                    <LeafText wide={false}> Triage </LeafText>
+                </HStack>
+                <FlatContainer style={{ flex: 1, width: "100%" }}></FlatContainer>
+
+                <HStack>
+                    <LeafIcon icon={"clipboard-list-outline"} size={iconSize} color={LeafColors.textDark} />
+                    <LeafText wide={false}> Events </LeafText>
+                </HStack>
+                <FlatContainer style={{ flex: 1, width: "100%" }}></FlatContainer>
             </VStack>
         </DefaultScreenContainer>
     );
