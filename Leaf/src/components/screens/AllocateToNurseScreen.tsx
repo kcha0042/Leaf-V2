c7b3b02aba494eef22a435e533b212f365e90ed8
Nabeeb Yusuf <nabeeb2000@gmail.com>
2023-08-07 21:47:35 +1000

    Added search filter buttons to AllocateToNurseScreen

--- Leaf/src/components/screens/AllocateToNurseScreen.tsx
+++ Leaf/src/components/screens/AllocateToNurseScreen.tsx
@@ -6,7 +6,7 @@ import React, { useEffect } from "react";
 import Patient from "../../model/patient/Patient";
 import Session from "../../model/Session";
 import StateManager from "../../state/publishers/StateManager";
-import { FlatList, View } from "react-native";
+import { FlatList } from "react-native";
 import AllocateToNurseCard from "../custom/AllocatePatientToNurseCard";
 import LeafDimensions from "../styling/LeafDimensions";
 import LeafSearchBarNew from "../base/LeafSearchBar/LeafSearchBarNew";
@@ -14,6 +14,11 @@ import LeafSegmentedButtons from "../base/LeafSegmentedButtons/LeafSegmentedButt
 import LeafSegmentedValue from "../base/LeafSegmentedButtons/LeafSegmentedValue";
 import { strings } from "../../localisation/Strings";
 import LeafButton from "../base/LeafButton/LeafButton";
+import HStack from "../containers/HStack";
+import HGap from "../containers/layout/HGap";
+import LeafTypography from "../styling/LeafTypography";
+import { LeafButtonType } from "../base/LeafButton/LeafButtonType";
+import LeafColors from "../styling/LeafColors";
 
 interface Props {
     navigation?: NavigationProp<ParamListBase>;
@@ -38,23 +43,79 @@ const AllocateToNurseScreen: React.FC<Props> = ({ navigation }) => {
     const onSetSegmentedValue = (segmentedValue) => {
         // TODO: Filter patients by time of day
         setSegmentedValue(segmentedValue);
-    }
-    const [shouldShowTime,setShouldShowTime] = React.useState(false);
-    const [shouldShowCode,setShouldShowCode] = React.useState(false);
+    };
+    const [shouldShowTime, setShouldShowTime] = React.useState(false);
+    const [shouldShowCode, setShouldShowCode] = React.useState(false);
     return (
         <DefaultScreenContainer>
             <VStack>
-                <LeafSearchBarNew onTextChange={onSearch}/>
+                <LeafSearchBarNew onTextChange={onSearch} />
                 <VGap size={20} />
-                <View>
-                <LeafButton label={strings("searchBarFilter.time")} onPress={ () => setShouldShowTime(!shouldShowTime)} wide={false}></LeafButton>
-                { shouldShowTime ? (<LeafSegmentedButtons label={strings("searchBarFilter.time")} options={ [new LeafSegmentedValue(0, "Morning"), new LeafSegmentedValue(1, "Noon"), new LeafSegmentedValue(2, "Afternoon"), new LeafSegmentedValue(3, "None")] } value={segmentedValue} onSetValue={setSegmentedValue} ></LeafSegmentedButtons>): null }
-                <VGap size={20} />
-                <LeafButton label={strings("searchBarFilter.triageCode")} onPress={ () => setShouldShowCode(!shouldShowCode)} wide={false}></LeafButton>
-                { shouldShowCode ? (<LeafSegmentedButtons label={strings("searchBarFilter.triageCode")} options={ [new LeafSegmentedValue(0, "1"), new LeafSegmentedValue(1, "2"), new LeafSegmentedValue(2, "3"), new LeafSegmentedValue(3, "4"), new LeafSegmentedValue(4, "5")] } value={segmentedValue} onSetValue={setSegmentedValue} ></LeafSegmentedButtons>): null }
-                </View>
-                <VGap size={6} />
-                
+                <HStack>
+                    <LeafButton
+                        label={strings("searchBarFilter.time")}
+                        onPress={() => setShouldShowTime(!shouldShowTime)}
+                        typography={LeafTypography.buttonSmall}
+                        type={LeafButtonType.Filled}
+                        color={LeafColors.transparent}
+                        wide={false}
+                        style={{
+                            alignSelf: "center",
+                            borderRadius: 15,
+                            marginRight: 1,
+                            borderWidth: 1,
+                            borderColor: "#3f4169",
+                        }}
+                    ></LeafButton>
+
+                    <HGap size={6} />
+                    <LeafButton
+                        label={strings("searchBarFilter.triageCode")}
+                        onPress={() => setShouldShowCode(!shouldShowCode)}
+                        typography={LeafTypography.buttonSmall}
+                        type={LeafButtonType.Filled}
+                        color={LeafColors.transparent}
+                        wide={false}
+                        style={{
+                            alignSelf: "center",
+                            borderRadius: 15,
+                            marginRight: 1,
+                            borderWidth: 1,
+                            borderColor: "#3f4169",
+                        }}
+                    ></LeafButton>
+                </HStack>
+                <VStack>
+                    <VGap size={6} />
+                    {shouldShowTime ? (
+                        <LeafSegmentedButtons
+                            label={strings("searchBarFilter.time")}
+                            options={[
+                                new LeafSegmentedValue(0, "Morning"),
+                                new LeafSegmentedValue(1, "Noon"),
+                                new LeafSegmentedValue(2, "Afternoon"),
+                                new LeafSegmentedValue(3, "None"),
+                            ]}
+                            value={segmentedValue}
+                            onSetValue={setSegmentedValue}
+                        ></LeafSegmentedButtons>
+                    ) : null}
+                    <VGap size={6} />
+                    {shouldShowCode ? (
+                        <LeafSegmentedButtons
+                            label={strings("searchBarFilter.triageCode")}
+                            options={[
+                                new LeafSegmentedValue(0, "1"),
+                                new LeafSegmentedValue(1, "2"),
+                                new LeafSegmentedValue(2, "3"),
+                                new LeafSegmentedValue(3, "4"),
+                                new LeafSegmentedValue(4, "5"),
+                            ]}
+                            value={segmentedValue}
+                            onSetValue={setSegmentedValue}
+                        ></LeafSegmentedButtons>
+                    ) : null}
+                </VStack>
                 <VGap size={20} />
                 <FlatList
                     data={patients}
