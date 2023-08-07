f576a43f4e21b628c468144856fe6c11982cd7a6
Andre Pham <andrepham33@gmail.com>
2023-08-07 14:23:04 +1000

    Created connectino to firebase

---
+++ Leaf/firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Firebase config
+const firebaseConfig = {
+apiKey: "AIzaSyD9cKsA2JghkrDHNhOvBmqOUPa8_jx-Dg4",
+authDomain: "leaf-f184f.firebaseapp.com",
+projectId: "leaf-f184f",
+storageBucket: "leaf-f184f.appspot.com",
+messagingSenderId: "958929199285",
+appId: "1:958929199285:web:473b2d7be90466e3659efc",
+measurementId: "G-49C9WMRJM1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
