122de61baabb89848c9a0f50a46043084cebf3e1
Andre Pham <andrepham33@gmail.com>
2023-08-14 20:36:24 +1000

    Added comments to ShiftTime

--- Leaf/src/model/employee/ShiftTime.ts
+++ Leaf/src/model/employee/ShiftTime.ts
@@ -1,8 +1,11 @@
 import { strings } from "../../localisation/Strings";
 
 export class ShiftTime {
+    // Morning shift, 7am - 3pm
     public static morning = new ShiftTime("MORNING");
+    // Afternoon shift, 3pm - 11pm
     public static afternoon = new ShiftTime("AFTERNOON");
+    // Night shift, 11pm - 7am
     public static night = new ShiftTime("AFTERNOON");
 
     public readonly id: string;
