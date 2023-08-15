78d7a332e1e59caae50e3bdf9ec5dc5b1f2f03af
Andre Pham <andrepham33@gmail.com>
2023-08-15 18:07:10 +1000

    Added commands to get a single worker or patient from the database

--- Leaf/src/model/session/GetPatientsManager.ts
+++ Leaf/src/model/session/GetPatientsManager.ts
@@ -1,6 +1,7 @@
 import { DatabaseCollection } from "../../database/DatabaseCollection";
 import DatabaseSession from "../../database/DatabaseSession";
 import PatientDataObject from "../../database/PatientDataObject";
+import MRN from "../patient/MRN";
 import Patient from "../patient/Patient";
 
 class GetPatientsManager {
@@ -11,6 +12,11 @@ class GetPatientsManager {
     public async getPatients(): Promise<Patient[]> {
         const patientDataObjects = await DatabaseSession.inst.readCollection(DatabaseCollection.Patients);
         return patientDataObjects.map((data) => PatientDataObject.restore(data));
+    }
+
+    public async getPatient(mrn: MRN): Promise<Patient | null> {
+        const dataObject = await DatabaseSession.inst.read(DatabaseCollection.Patients, mrn.toString());
+        return PatientDataObject.restore(dataObject);
     }
 }
 
--- Leaf/src/model/session/GetWorkersManager.ts
+++ Leaf/src/model/session/GetWorkersManager.ts
@@ -1,6 +1,7 @@
 import { DatabaseCollection } from "../../database/DatabaseCollection";
 import DatabaseSession from "../../database/DatabaseSession";
 import WorkerDataObject from "../../database/WorkerDataObject";
+import EmployeeID from "../employee/EmployeeID";
 import Worker from "../employee/Worker";
 
 class GetWorkersManager {
@@ -9,8 +10,13 @@ class GetWorkersManager {
     private constructor() {}
 
     public async getWorkers(): Promise<Worker[]> {
-        const workerDataObjects = await DatabaseSession.inst.readCollection(DatabaseCollection.Workers);
-        return workerDataObjects.map((data) => WorkerDataObject.restore(data));
+        const dataObjects = await DatabaseSession.inst.readCollection(DatabaseCollection.Workers);
+        return dataObjects.map((data) => WorkerDataObject.restore(data));
+    }
+
+    public async getWorker(id: EmployeeID): Promise<Worker | null> {
+        const dataObject = await DatabaseSession.inst.read(DatabaseCollection.Workers, id.toString());
+        return WorkerDataObject.restore(dataObject);
     }
 }
 
