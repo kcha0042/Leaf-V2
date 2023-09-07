11c6e61e3b8fd8c01d75324f5d46f99c74eaa69b
Andre Pham <andrepham33@gmail.com>
2023-09-07 17:33:13 +1000

    Extracted export patient functionality into its own util

--- Leaf/src/components/screens/ExportPatientScreen.tsx
+++ Leaf/src/components/screens/ExportPatientScreen.tsx
@@ -1,90 +1,31 @@
-import React, { useEffect, useState } from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import VGap from "../containers/layout/VGap";
 import { NavigationProp, ParamListBase } from "@react-navigation/native";
-import VStack from "../containers/VStack";
-import HStack from "../containers/HStack";
-import DefaultScreenContainer from "./containers/DefaultScreenContainer";
-import Session from "../../model/session/Session";
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { strings } from "../../localisation/Strings";
 import Patient from "../../model/patient/Patient";
-import StateManager from "../../state/publishers/StateManager";
-import { FlatList, ScrollView, View } from "react-native";
-import LeafDimensions from "../styling/LeafDimensions";
-import ExportPatientCard from "../custom/ExportPatientCard";
-import * as FileSystem from "expo-file-system";
-import { shareAsync } from "expo-sharing";
+import Session from "../../model/session/Session";
 import Environment from "../../state/environment/Environment";
 import { OS } from "../../state/environment/types/OS";
-import LeafTypography from "../styling/LeafTypography";
+import StateManager from "../../state/publishers/StateManager";
 import LeafButton from "../base/LeafButton/LeafButton";
 import { LeafButtonType } from "../base/LeafButton/LeafButtonType";
-import LeafColors from "../styling/LeafColors";
-import { strings } from "../../localisation/Strings";
-import LeafText from "../base/LeafText/LeafText";
 import LeafCheckboxStatic from "../base/LeafCheckbox/LeafCheckboxStatic";
+import LeafText from "../base/LeafText/LeafText";
+import HStack from "../containers/HStack";
+import VStack from "../containers/VStack";
+import VGap from "../containers/layout/VGap";
+import ExportPatientCard from "../custom/ExportPatientCard";
+import LeafColors from "../styling/LeafColors";
+import LeafDimensions from "../styling/LeafDimensions";
+import LeafTypography from "../styling/LeafTypography";
 import { LeafFontWeight } from "../styling/typography/LeafFontWeight";
+import DefaultScreenContainer from "./containers/DefaultScreenContainer";
+import { exportPatient } from "../../utils/ExportPatientUtil";
 
 interface Props {
     navigation?: NavigationProp<ParamListBase>;
 }
-
-const exportPatient = async (selectedPatients: Patient[]) => {
-    if (selectedPatients.length == 0) {
-        return;
-    }
-    const date = new Date();
-    const dateString = date.toLocaleString();
-    // Define a regular expression to match white spaces, commas, colons, and slashes.
-    const regex = /[,\s:\/]/g;
-    // Replace white spaces, commas, colons, and slashes with underscores.
-    const sanitizedDatestring = dateString.replace(regex, "_");
-
-    // Generate file.
-    const filename = `${sanitizedDatestring}.csv`; // Assuming the file name is the date time
-    var csvData = "MRN,DOB,FirstName,LastName,Gender,PhoneNumber,PostCode,TimeLastAllocated,AllocatedTo,Events\n";
-    for (const patient of selectedPatients) {
-        var allEvents = "";
-        for (const event of patient.events) {
-            allEvents += `[${event.getExportSummary()}]`;
-        }
-        csvData += `${patient.mrn},${patient.dob},${patient.firstName},${patient.lastName},${patient.sex},${patient.phoneNumber},${patient.postCode},${patient.timeLastAllocated},${patient.idAllocatedTo},${allEvents}\n`;
-    }
-
-    if (Environment.inst.getOS() == OS.Android) {
-        const permission = await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync(); // Getting permission for android.
-        if (permission.granted) {
-            await FileSystem.StorageAccessFramework.createFileAsync(permission.directoryUri, filename, "csv")
-                .then(async (uri) => {
-                    await FileSystem.writeAsStringAsync(uri, csvData, {
-                        encoding: FileSystem.EncodingType.UTF8,
-                    });
-                })
-                .catch((e) => console.log(e));
-        } else {
-            console.log("Permission denied");
-        }
-    } else if (Environment.inst.getOS() == OS.IOS) {
-        // Define a regular expression to match white spaces, commas, colons, and slashes
-        const regex = /[,\s:\/]/g;
-        // Replace white spaces, commas, colons, and slashes with underscores
-        const sanitizedFileName = filename.replace(regex, "_");
-        const filePath = FileSystem.documentDirectory + sanitizedFileName;
-
-        try {
-            await FileSystem.writeAsStringAsync(filePath, csvData, { encoding: FileSystem.EncodingType.UTF8 });
-            await shareAsync(filePath);
-        } catch (e) {
-            console.log(e);
-        }
-    } else if (Environment.inst.getOS() == OS.Web) {
-        const blob = new Blob([csvData], { type: "text/csv" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = filename;
-        a.click();
-    }
-};
 
 const ExportPatientScreen: React.FC<Props> = ({ navigation }) => {
     const [patients, setPatients] = React.useState<Patient[]>(Session.inst.getAllPatients());
