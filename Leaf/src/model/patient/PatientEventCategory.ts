80cf2b474e7d09a79463c06d356943452570a895
Alex Zhou <azho0003@student.monash.edu>
2023-10-05 20:53:59 +1100

    Updated event categories to reflect OMOP model

--- Leaf/src/model/patient/PatientEventCategory.ts
+++ Leaf/src/model/patient/PatientEventCategory.ts
@@ -1,8 +1,15 @@
 import { strings } from "../../localisation/Strings";
 
 export class PatientEventCategory {
-    public static medication = new PatientEventCategory("MEDICATION");
-    public static other = new PatientEventCategory("OTHER");
+    public static medication = new PatientEventCategory("DRUG EXPOSURE");
+    public static visit = new PatientEventCategory("VISIT OCCURRENCE");
+    public static condition = new PatientEventCategory("CONDITION OCCURRENCE");
+    public static procedure = new PatientEventCategory("PROCEDURE OCCURRENCE");
+    public static device = new PatientEventCategory("DEVICE EXPOSURE");
+    public static measurement = new PatientEventCategory("MEASUREMENT");
+    public static observation = new PatientEventCategory("OBSERVATION");
+    public static episode = new PatientEventCategory("EPISODE");
+    public static note = new PatientEventCategory("NOTE");
 
     public readonly id: string;
 
@@ -18,8 +25,29 @@ export class PatientEventCategory {
         if (this.matches(PatientEventCategory.medication)) {
             return strings("patientEventCategory.medication");
         }
-        if (this.matches(PatientEventCategory.other)) {
-            return strings("patientEventCategory.other");
+        if (this.matches(PatientEventCategory.visit)) {
+            return strings("patientEventCategory.visit");
+        }
+        if (this.matches(PatientEventCategory.condition)) {
+            return strings("patientEventCategory.condition");
+        }
+        if (this.matches(PatientEventCategory.procedure)) {
+            return strings("patientEventCategory.procedure");
+        }
+        if (this.matches(PatientEventCategory.device)) {
+            return strings("patientEventCategory.device");
+        }
+        if (this.matches(PatientEventCategory.measurement)) {
+            return strings("patientEventCategory.measurement");
+        }
+        if (this.matches(PatientEventCategory.observation)) {
+            return strings("patientEventCategory.observation");
+        }
+        if (this.matches(PatientEventCategory.episode)) {
+            return strings("patientEventCategory.episode");
+        }
+        if (this.matches(PatientEventCategory.note)) {
+            return strings("patientEventCategory.note");
         }
         return strings("unknown");
     }
