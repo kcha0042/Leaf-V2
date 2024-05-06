eb84f11378899996a1cb58960a10039a74dac756
czub0002 <czub0002@student.monash.edu>
2024-05-06 13:11:54 +1000

    Updated UI

--- Leaf/src/components/base/LeafIcon/LeafIcon.tsx
+++ Leaf/src/components/base/LeafIcon/LeafIcon.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ViewStyle } from "react-native";
+import { ViewStyle, TouchableOpacity } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import LeafColor from "../../styling/color/LeafColor";
 
@@ -12,10 +12,21 @@ interface Props {
     size: number;
     // Custom style
     style?: ViewStyle;
+    // Event handler for when the icon is pressed
+    onPress?: () => void;  // Add the onPress prop
 }
 
-const LeafIcon: React.FC<Props> = ({ icon, color, size, style }) => {
-    return <Icon name={icon} size={size} color={color.getColor()} style={style} />;
+const LeafIcon: React.FC<Props> = ({ icon, color, size, style, onPress }) => {
+    return (
+        <TouchableOpacity onPress={onPress}>
+            <Icon
+                name={icon}
+                size={size}
+                color={color.getColor()}
+                style={style}
+            />
+        </TouchableOpacity>
+    );
 };
 
 export default LeafIcon;
